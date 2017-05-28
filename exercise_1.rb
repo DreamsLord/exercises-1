418018bed1d5d2658d73287632e1f478450573db
DreamsLord <adamludian@gmail.com>
2017-05-28 20:31:22 +0200

    algorithm done

--- exercise_1.rb
+++ exercise_1.rb
@@ -8,6 +8,29 @@ def odd2(num)
 ((1..num).map{ |n| n if n.odd?}).compact.sum
 end
 
-puts odd_sum(10)
-p odd2(10)
-puts odd2(1)
+def odd3(num)
+  num.odd? ? n = num + 1 : n = num
+  n /= 2
+  ((1 + num) / 2 ) * n
+end
+
+
+def simple_b
+  start = Time.now
+  yield
+  return Time.now - start
+end
+
+def odd_sum(n)
+  ###
+end
+
+
+puts odd2(9)
+puts odd3(9)
+puts odd2(55)
+puts odd3(55)
+puts "=============="
+puts simple_b { odd3(100) }
+puts simple_b { odd3(100_000) }
+puts simple_b { odd3(100_000_000) }
